ce30524954bca58d9f29d9a72e88a71e90bca592
Tarun Mittal <tm6622@g.rit.edu>
2023-05-16 04:22:39 -0400

    added custom hn func

--- scrape.py
+++ scrape.py
@@ -5,3 +5,12 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline')
 votes = soup.select('.score')
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        hn.append(title)
+    return hn
+
+create_custom_hn(links, votes)
